8e2957a479f3f396180d01b6ed7abdc71dd396f6
rajendra <nagabhoinarajendra@gmail.com>
2018-09-11 07:44:25 +0530

    Date picker

--- DatePicker/src/main/java/com/datepicker/Page.java
+++ DatePicker/src/main/java/com/datepicker/Page.java
@@ -1,35 +1,108 @@
 package com.datepicker;
-
+/**
+ * @author Rajendra_nagaboina
+ */
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+/**
+ * Basic functionality of Date picker Home page.
+ */
 public class Page {
-
+	/**
+	 * Web Browser driver object.
+	 */
 	WebDriver driver;
-	
-	static int number = 0;
-	@FindBy(xpath = "//*[@id='example-0']/div/div/div/div/div/div/div/div/div[@class = 'react-datepicker__day react-datepicker__day--sat react-datepicker__day--weekend' and text() = '15']")
-	WebElement pick;
-	String[] pickDate = new String [] {"example-0", "example-1", "example-2", "example-3", "example-4", "example-5"};
+	/**
+	 * path for text box with date.
+	 */
+	@FindBy(xpath = "//*[@id='example-0']/div/div/div/div/div/input")
+	WebElement picker;
+	/**
+	 * path for previous month button.
+	 */
+	@FindBy(xpath = "//*[@id = 'example-0']//*[@class = 'react-datepicker__navigation react-datepicker__navigation--previous']")
+	private WebElement previousMonth;
+	/**
+	 * path for next month button.
+	 */
+	@FindBy(xpath = "//*[@id = 'example-0']//*[@class = 'react-datepicker__navigation react-datepicker__navigation--next']")
+	private WebElement nextMonth;
+	/**
+	 * Page constructor that initializes elements in page.
+	 * @param driver of browser
+	 */
 	public Page(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
-	public void datePicker() {
-		while(number < 6) {
-		if(number == 2) {
-			number++;
-			continue;
+	/**
+	 * Clicks on the date provided.
+	 * @param requestedDate date that uses wants to navigate
+	 */
+	public void datePicker(String requestedDate) {
+		picker.click();
+		String currentDate = picker.getAttribute("value");
+		int currentMonth = Integer.parseInt(currentDate.substring(0, 2));
+		int currentYear = Integer.parseInt(currentDate.substring(6));
+		int requestedMonth = Integer.parseInt(requestedDate.substring(0, 2));
+		int requestedYear = Integer.parseInt(requestedDate.substring(6));
+		int yearClicks = (requestedYear - currentYear);
+		int monthClicks = (requestedMonth - currentMonth);
+		int requestedDay = Integer.parseInt(requestedDate.substring(3, 5));
+		clickYears(yearClicks,monthClicks,requestedDay);
+	}
+	/**
+	 * navigates to the requested year.
+	 * @param yearClicks difference in current and requested years
+	 * @param monthClicks difference in current and requested Months
+	 * @param day that is requested
+	 */
+	private void clickYears(int yearClicks,int monthClicks, int day) {
+		if(yearClicks >= 0) {
+			int clicks = yearClicks * 12;
+			while(clicks != 0) {
+				nextMonth.click();
+				clicks--;
+			}
+			clickMonths(monthClicks,day);
+		} else {
+			int clicks = yearClicks * 12;
+			while(clicks != 0) {
+				previousMonth.click();
+				clicks++;
+			}
+			clickMonths(monthClicks,day);
 		}
-		WebElement element = driver.findElement(By.xpath("//*[@id='" + pickDate[number] +"']/div/div/div/div/div/input"));
-		element.click();
-		WebElement ele = driver.findElement(By.xpath("//*[@id='" + pickDate[number] +"']/div/div/div/div/div/div/div/div/div[@class = 'react-datepicker__day react-datepicker__day--sat react-datepicker__day--weekend' and text() = '15']"));
-		ele.click();
-		number ++;
+	}
+	/**
+	 * Navigates to the current month.
+	 * @param monthClicks difference in current and requested Months
+	 * @param day that is requested
+	 */
+	private void clickMonths(int monthClicks, int day) {
+		if(monthClicks >= 0) {
+			while(monthClicks != 0) {
+				nextMonth.click();
+				monthClicks--;
+			}
+			clickDate(day);
+		} else {
+			while(monthClicks != 0) {
+				previousMonth.click();
+				monthClicks++;
+			}
+			clickDate(day);
 		}
+	}
+	/**
+	 * clicks on requested date.
+	 * @param day requested day by user
+	 */
+	private void clickDate(int day) {
+		WebElement date = driver.findElement(By.xpath("//*[@id='example-0']/div/div/div/div/div/div/div/div/div[text() = '"+day+"']"));
+		date.click();
 	}
 }
--- DatePicker/src/test/java/com/testDatepicker/testDatePicker.java
+++ DatePicker/src/test/java/com/testDatepicker/testDatePicker.java
@@ -1,25 +1,38 @@
-package com.datepicker;
+package com.testDatepicker;
+/**
+ * @author Rajendra_Nagaboina
+ */
+import static org.testng.Assert.assertEquals;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-public class testDatePicker {
 
+import com.datepicker.Page;
+public class testDatePicker {
+	/**
+	 * web browser object.
+	 */
 	WebDriver driver;
-	
+	/**
+	 * Initializes the web Browser.
+	 */
 	@BeforeClass
 	public void setProperties() {
-		 System.setProperty("webdriver.chrome.driver","C:\\Users\\Rajendra_Nagaboina\\Desktop\\Selenium Test Automation\\drivers\\chromedriver.exe");
+		 System.setProperty("webdriver.chrome.driver","/Users/rajendra/Desktop/TestAutomation/drivers/chromedriver");
 		 driver = new ChromeDriver();
+		 driver.get("https://reactdatepicker.com");
 	}
-	@Test(description = "launch the site", priority = 1)
-	public void launchSite() {
-		driver.get("https://reactdatepicker.com");
-	}
-	@Test(description = "picks the date", priority = 2)
+	/**
+	 * check for selected date is correct or not.
+	 */
+	@Test(description = "picks the date")
 	public void pickDate() {
+		String date = "08/16/2016";
 		Page page = new Page(driver);
-		page.datePicker();
+		page.datePicker(date);
+		assertEquals(driver.findElement(By.xpath("//*[@id='example-0']/div/div/div/div/div/input")).getAttribute("value"),date);
 	}
 }
